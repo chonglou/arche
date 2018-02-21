be7c8af3c7a7ff2499c20730112ffc4e24a97c1e
Jeremy Zheng <jitang.zheng@gmail.com>
2018-02-21 10:42:17 -0800

    add  materialize-css

--- plugins/nut/plugin.go
+++ plugins/nut/plugin.go
@@ -1,1 +1,25 @@
 package nut
+
+import (
+	"github.com/chonglou/arche/web/queue"
+	"github.com/garyburd/redigo/redis"
+)
+
+// Plugin plugin
+type Plugin struct {
+	I18n      *web.I18n     `inject:""`
+	Cache     *web.Cache    `inject:""`
+	Jwt       *web.Jwt      `inject:""`
+	Jobber    *queue.Queue  `inject:""`
+	Settings  *web.Settings `inject:""`
+	Security  *web.Security `inject:""`
+	S3        *web.S3       `inject:""`
+	Sitemap   *web.Sitemap  `inject:""`
+	RSS       *web.RSS      `inject:""`
+	Router    *gin.Engine   `inject:""`
+	DB        *gorm.DB      `inject:""`
+	Redis     *redis.Pool   `inject:""`
+	Dao       *Dao          `inject:""`
+	Layout    *Layout       `inject:""`
+	Languages []string      `inject:"languages"`
+}
