3a75103bbe44f13338ae8daeb5660baac9429d35
Jeremy Zheng <jitang.zheng@gmail.com>
2018-02-27 16:42:49 -0800

    remove frontend

--- frontend/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../layouts/application'
-
-export default() => (<Layout>
-  <div>
-    <hr/>
-    home {process.env.BACKEND}
-    <img src='/static/fail.png'/>
-  </div>
-</Layout>)
